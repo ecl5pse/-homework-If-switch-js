5defdec67cc09dfac7765e8a073b0c05da13f3a4
ecl5pse <fantamas20@gmail.com>
2019-11-13 00:26:45 +0200

    made in sixth task

--- assers/js/index.js
+++ assers/js/index.js
@@ -105,6 +105,69 @@ if ( userCoordinateX > 0 && userCoordinateY > 0   ){
 
 */
 
+/* Шестое задание
+
+const  userValueMonth = prompt("Plese enter your number month");
+const  userNumberMonth  = +userValueMonth;
+
+switch (userNumberMonth) {
+
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    case 7:
+        console.log("July");
+        break;
+
+    case 8:
+        console.log("August");
+        break;
+
+    case 9:
+        console.log("September");
+        break;
+
+    case 10:
+        console.log("October");
+        break;
+
+    case 11:
+        console.log("November");
+        break;
+
+    case 12:
+        console.log("December");
+        break;
+
+     default:
+        console.log("Enter number 1 to 12");
+        break;
+
+}
+
+
+*/
+
 
 
 
